6279bb0942e122b6654c5303ad7fc84c3f77175d
AginS <agins.main@gmail.com>
2021-05-26 14:44:07 +0300

    Initial commit

---
+++ inst-clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  inst-clone
+//
+//  Created by Vlad Vrublevsky on 26.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ inst-clone/inst_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  inst_cloneApp.swift
+//  inst-clone
+//
+//  Created by Vlad Vrublevsky on 26.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct inst_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
