ca191e7531e98a60530a0bf7e1ced96b86c1f64f
AginS <agins.main@gmail.com>
2021-05-27 12:47:42 +0300

    попытка в кэш #2

--- inst-clone/InstCore.swift
+++ inst-clone/InstCore.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Alamofire
+import AlamofireURLCache5
 import SwiftUI
 import UIKit
 
@@ -18,17 +19,20 @@ class InstCore: ObservableObject {
     
     func load() {
         let cacher = ResponseCacher(behavior: .cache)
-        AF.request("https://picsum.photos/v2/list")
+       
+        AF.request("https://picsum.photos/v2/list", refreshCache: false)
             .cacheResponse(using: cacher)
             .responseDecodable(of: [PicsumItem].self) { responce in
-            switch responce.result {
-            case .success(let items):
-                self.items = items
-                self.idForUpdate = items[items.count-5].id
-            case .failure(let error):
-                print(error)
+                debugPrint(responce)
+                switch responce.result {
+                case .success(let items):
+                    self.items = items
+                    self.idForUpdate = items[items.count-5].id
+                case .failure(let error):
+                    print(error)
+                }
             }
-        }
+            .cache(maxAge: 600, isPrivate: false, ignoreServer: true)
     }
     
     func loadNewImages() {
